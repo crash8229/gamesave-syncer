af26148489e233952626b501007b95c36cabb1e8
Darren M. OConnell <doconnell@cursived.com>
2019-04-12 23:41:08 -0700

    changed user

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ class App:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname='mikeuserver.zapto.org', port=5555,
-                       username='crash8229', key_filename='private')
+                       username='gamesaver', key_filename='private')
         stdin, stdout, stderr = client.exec_command('ls -l')
         out = stdout.read().decode("UTF-8")
 
